3886dd2f1e8c772df8f65fc809984b22dd2824f9
balaji10krishnan <balaji10krishnan@gmail.com>
2025-02-07 20:59:48 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,129 @@
+import { useState, useEffect } from "react";
+import { Close, Logo, Menu } from "../../assets/svg/svg";
+import classes from "./Header.module.css";
+
+const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isSideMenu, setIsSideMenu] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className={`${isScrolled || isSideMenu ? classes["scrolled"] : ""} ${
+          classes["header-bg"]
+        }`}
+      >
+        <header className={`container ${classes["header"]}`}>
+          <a className={`${classes["logo"]}`}>
+            <Logo
+              fill={
+                isScrolled || isSideMenu
+                  ? "var(--clr-black)"
+                  : "var(--clr-white)"
+              }
+            />
+          </a>
+          <div className={`${classes["menu"]}`}>
+            <ul className={`${classes["menu-list"]}`}>
+              <li className={` f-m ${classes["menu-item"]}`}>
+                <a href="">Why Hexnode</a>
+              </li>
+              <li className={` f-m ${classes["menu-item"]}`}>
+                <a href="">Features</a>
+              </li>
+              <li className={` f-m ${classes["menu-item"]}`}>
+                <a href="">Platforms</a>
+              </li>
+              <li className={` f-m ${classes["menu-item"]}`}>
+                <a href="">Customers</a>
+              </li>
+            </ul>
+
+            <div className={`${classes["trail-plan"]}`}>
+              <button className={`f-m f-b btn-red ${classes["trail-btn"]}`}>
+                14 days free trail
+              </button>
+            </div>
+          </div>
+          <div
+            className={`${classes["menu-icon"]}`}
+            onClick={() => {
+              setIsSideMenu((pre) => !pre);
+              if (isSideMenu) {
+                document.body.style.overflow = "auto";
+              } else {
+                document.body.style.overflow = "hidden";
+              }
+            }}
+          >
+            {isSideMenu ? (
+              <Close fill={"var(--clr-black)"} stroke={"var(--clr-black)"} />
+            ) : (
+              <Menu
+                fill={
+                  isScrolled || isSideMenu
+                    ? "var(--clr-black)"
+                    : "var(--clr-white)"
+                }
+                stroke={
+                  isScrolled || isSideMenu
+                    ? "var(--clr-black)"
+                    : "var(--clr-white)"
+                }
+              />
+            )}
+          </div>
+        </header>
+      </div>
+      <div
+        className={`${classes["side-menu"]} ${
+          isSideMenu ? classes["active"] : ""
+        }`}
+      >
+        <div className={`${classes["side-bar"]}`}>
+          <div
+            className={`${classes["side-menu-item"]}`}
+            onClick={() => {
+              setIsSideMenu((pre) => !pre);
+              if (isSideMenu) {
+                document.body.style.overflow = "auto";
+              } else {
+                document.body.style.overflow = "hidden";
+              }
+            }}
+          >
+            Close
+          </div>
+          <div className={`${classes["side-menu-item"]}`}>
+            <a href="">Why Hexnode</a>
+          </div>
+          <div className={`${classes["side-menu-item"]}`}>
+            <a href="">Features</a>
+          </div>
+          <div className={`${classes["side-menu-item"]}`}>
+            <a href="">Platforms</a>
+          </div>
+          <div className={`${classes["side-menu-item"]}`}>
+            <a href="">Customers</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
